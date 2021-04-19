daeabe91bb6b1345198f972d957915e4f983499c
jiangpp <jiangpengpengv@126.coms>
2021-04-19 22:14:02 +0800

    Change Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT");
 		System.out.println("www.mldn.cn");
-		System.out.println("good job");
+		System.out.println("good job");	
+		System.out.println("nihao");		
 	}
 }
